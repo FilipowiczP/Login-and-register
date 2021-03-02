0de869fb7eddf8cc58620b8e86d1d713da2a9e36
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2021-03-02 19:54:53 +0100

    complete

--- view/src/components/Logout/Logout.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './logout.scss';
-
-const Logout = () => {
-  return <h1 className="logout">Logged out successfully </h1>;
-};
-
-export default Logout;
--- view/src/components/validation.ts
+++
@@ -1,2 +0,0 @@
-export const LOGIN_LENGTH: number = 4;
-export const PASSWORD_LENGTH: number = 5;
